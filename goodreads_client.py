aaa8a5257372246b354f7b15daec659cf8d064bb
chitrakakkar <chitrakakkar@yahoo.co.in>
2016-10-11 23:27:37 -0500

    Initial Multi-threading coded.

--- goodreads_client.py
+++ goodreads_client.py
@@ -10,34 +10,45 @@ class goodreads_client():
 
     @staticmethod
     def search(parameter):
-        # We construct a URL to send to The Goodreads API.  In this case it will take our key, and a
-        # search parameter, either a Author, ISBN, or title. in my example file 'test_client.py'
-        # you will see that I send the parameter 'Ender' and get a bucnh of weird HTML tags back.
-        base_url = 'https://www.goodreads.com/search/index.xml?q='+parameter+'&key='+secret_key
-        # An API call MUST be use .post not .get, because that tells the server we expect something back.
-        response = requests.post(base_url)
-        # here we make an Element Form of our XML so we can iterate through it to get information
-        dict_form = ET.fromstring(response.text)
-        # This should let me iterate through and get what I want.
-        results = parse_best_books(dict_form)
-        return results
+        try:
+            # We construct a URL to send to The Goodreads API.  In this case it will take our key, and a
+            # search parameter, either a Author, ISBN, or title. in my example file 'test_client.py'
+            # you will see that I send the parameter 'Ender' and get a bucnh of weird HTML tags back.
+            base_url = 'https://www.goodreads.com/search/index.xml?q='+parameter+'&key='+secret_key
+            # An API call MUST be use .post not .get, because that tells the server we expect something back.
+            response = requests.post(base_url)
+            # here we make an Element Form of our XML so we can iterate through it to get information
+            dict_form = ET.fromstring(response.text)
+            # This should let me iterate through and get what I want.
+            results = parse_best_books(dict_form)
+            return results
+        except Exception as e:
+            print("Invalid search keyword !!! ", e)
 
     @staticmethod
     def all_books_by_author(auth_id):
-        base_url = 'https://www.goodreads.com/series/list/'+auth_id+'.xml?key='+secret_key
-        response = requests.post(base_url)
-        dict_form = ET.fromstring(response.text)
-        results = parse_best_books(dict_form)
-        return results
+        try:
+            base_url = 'https://www.goodreads.com/series/list/'+auth_id+'.xml?key='+secret_key
+            response = requests.post(base_url)
+            dict_form = ET.fromstring(response.text)
+            results = parse_best_books(dict_form)
+            return results
+        except:
+            return " No author found with that ID"
+
 
     @staticmethod
     def author_by_name(author_name):
-        base_url = 'https://www.goodreads.com/api/author_url/' + author_name + '?key=' + secret_key
-        respond = requests.get(base_url)
-        dict_form = ET.fromstring(respond.text)
-        print("Dictionary", dict_form[1].attrib['id'])
-        result = parse_author(dict_form)
-        return result
+        try:
+            base_url = 'https://www.goodreads.com/api/author_url/' + author_name + '?key=' + secret_key
+            respond = requests.get(base_url)
+            dict_form = ET.fromstring(respond.text)
+            print("Dictionary", dict_form[1].attrib['id'])
+            result = parse_author(dict_form)
+            return result
+        except Exception as e:
+            print("Invalid author-name !!!", e)
+
 
     @staticmethod
     def author_result_to_list(author_data):
@@ -45,6 +56,15 @@ class goodreads_client():
         for key, value in author_data.items():
             list_form.append(value)
         print(list_form)
+
+    @staticmethod
+    def get_author_info_by_id(author_id):
+        base_url = 'https://www.goodreads.com/author/show.xml ' + author_id + '?key=' + secret_key
+        respond = requests.get(base_url)
+        dict_form = ET.fromstring(respond.text)
+        print("Dictionary", dict_form[1].attrib['id'])
+        result = parse_author(dict_form)
+        return result
 
 
 def parse_author(dict_form):
--- main.py
+++ main.py
@@ -1,85 +1,34 @@
 """This file will run the programs main loops, show the displays and generally
    bring together the other files into a cohesive unit"""
-from tabulate import tabulate
 from Validator import *
 from goodreads_client import goodreads_client as ap
-from database import *
-from console_displays import menu_display
-from peewee_models import *
 import threading
 
 
-def show_menu():
-    """ displays the menu for the user
-    checks if user has chosen the right choice from the list
-    and calls methods to complete the action"""
-    search_thread = threading.Thread(name='search', targer=search_book,)
-    author_thread = threading.Thread(name='author_search', target=search_for_author)
-    while True:
-        menu = menu_display.main_menu()
-        menu_choice = int(input(menu))
-        while not is_whole_number(menu_choice, range(1, 4)):
-            menu_choice = int(input("Invalid entry, please select from the list !!!"))
-        if menu_choice == 1:
-            search_book()
-        if menu_choice == 2:
-            search_for_author()
-        if menu_choice ==3:
-            displaySavedBooks()
-        elif menu_choice == 4:
-           exit(4)
-
-
-def search_for_author():
-    author_name = get_string1_input("find auther by name")
-    author_data = ap.author_by_name(author_name)
+def search_for_author(keyword):
+    author_data = ap.author_by_name(keyword)
     print("ID: {}\nName: {}\nLink: {}\n".format(author_data['ID'], author_data['name'], author_data['link']))
-    insert_author_to_table(author_data)
 
 
-def search_book():
-    """Sub-menu for searching a book option."""
-    menu_string = menu_display.sub_menu()
-    while True:
-        menu_choice = get_user_int(menu_string)
-        if menu_choice == 1:
-            try:
-                keyword = get_string1_input('Enter author\'s name')
-                search_from_api(keyword)
-                break
-            except Exception as e:
-                print(" HERE - Error !!!", e)
-        elif menu_choice == 2:
-            keyword = get_string1_input("Enter ISBN for the book")
-            search_from_api(keyword)
-            break
-        elif menu_choice == 3:
-            keyword = get_string1_input("Enter the book-title")
-            search_from_api(keyword)
-            break
-        elif menu_choice == 4:
-            break
-        else:
-            print("Invalid input !!!")
+def search_book(keyword):
+    book_data = ap.all_books_by_author(keyword)
+    print(book_data)
 
 
-def search_from_api(keyword):
-    try:
-        Books = ap.search(keyword)
-        print("The books are:")
-        print(tabulate(Books, tablefmt="fancy_grid"))
-        for book in Books:
-                insert_books_to_table(book)
-    except Exception as e:
-        print("API Error !!! ", e)
+def search_book_title(keyword):
+    book_data = ap.search(keyword)
+    print(book_data)
 
 
 def main():
-    db.connect()
-    db.create_tables([book_model, author_model], safe=True)
-    menu_display.initial_console_display()
-    show_menu()
+    keyword = get_string1_input("Enter the search keyword !!!")
+    search_thread = threading.Thread(name='search', target=search_book, args=(keyword,))
+    author_thread = threading.Thread(name='author_search', target=search_for_author, args=(keyword,))
+    title_thread = threading.Thread(name='title', target=search_book_title, args=(keyword,))
 
+    search_thread.run()
+    author_thread.run()
+    title_thread.run()
 
 if __name__ == '__main__':
     main()
