707ddb57c3518088494c88153575daa0ea43af30
chitrakakkar <chitrakakkar@yahoo.co.in>
2016-10-14 13:24:45 -0500

    Initial Multi-threading coded.

--- main.py
+++ main.py
@@ -1,34 +1,63 @@
-"""This class does the multi-threading with a single search keyword
-Three different API calls from one key-word search with multi-threading"""
-from Validator import *
 from goodreads_client import goodreads_client as ap
 import threading
+from queue import Queue
+import time
+
+"""This is taken from: - https://pythonprogramming.net/threading-tutorial-python/
+  Reference given to me by -Anna Dudda !!!"""
+""" This class does the book-search on the basis of four different keywords given using multi-threading"""
+
+print_lock = threading.Lock()
 
 
-def search_for_author(keyword):
-    author_data = ap.author_by_name(keyword)
-    print("ID: {}\nName: {}\nLink: {}\n".format(author_data['ID'], author_data['name'], author_data['link']))
+def search_books(search):
+    book_data = ap.search(search)
+    time.sleep(.5)  # pretend  do some work.
+    with print_lock:
+        print("I am the book_data \n", book_data)
 
 
-def search_book(keyword):
-    book_data = ap.all_books_by_author(keyword)
-    print("book-search", book_data)
-
-
-def search_book_title(keyword):
-    book_data = ap.search(keyword)
+def search_book_title(search):
+    book_data = ap.author_by_name(search)
     print("title-search ", book_data)
 
 
-def main():
-    keyword = get_string1_input("Enter the search keyword !!!")
-    search_thread = threading.Thread(name='search', target=search_book, args=(keyword,))
-    author_thread = threading.Thread(name='author_search', target=search_for_author, args=(keyword,))
-    title_thread = threading.Thread(name='title', target=search_book_title, args=(keyword,))
+def threader():
+    while True:
+        # gets an worker from the queue
+        searcher = q.get()
+        # Run the example job with the avail worker in queue (thread)
+        search_books(searcher)
+        search_book_title(searcher)
+        # completed with the job
+        q.task_done()
 
-    search_thread.run()
-    author_thread.run()
-    title_thread.run()
+q = Queue()
 
-if __name__ == '__main__':
-    main()
+# how many threads are we going to allow for
+for x in range(5):
+    book_search = threading.Thread(name='book-search', target=threader)
+    author_search = threading.Thread(name='author_search', target=threader)
+    # classifying as a daemon, so they will die when the main dies
+    book_search.daemon = True
+    # begins, must come after daemon definition
+    author_search.daemon = True
+
+    book_search.start()
+    author_search.start()
+
+start = time.time()
+
+
+search_string = ['Computer', 'Love', 'Music', 'Violence']
+for search in search_string:
+    q.put(search)
+
+# 5 jobs assigned.
+for search in range(5):
+    q.put(search)
+
+# wait until the thread terminates.
+q.join()
+
+print('Entire job took:', time.time() - start)
