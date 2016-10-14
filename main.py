dba1bd64b18e7db01e44bba3d911521c8c1dcb85
chitrakakkar <chitrakakkar@yahoo.co.in>
2016-10-14 13:38:44 -0500

    Initial Multi-threading coded.

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from queue import Queue
 import time
 
 """This is taken from: - https://pythonprogramming.net/threading-tutorial-python/
-  Reference given to me by -Anna Dudda !!!"""
+  Reference given to me by - Anna Dudda !!!"""
 """ This class does the book-search on the basis of four different keywords given using multi-threading"""
 
+# putting a lock so that no other funtion can call it when other is using
 print_lock = threading.Lock()
 
 
@@ -14,12 +15,13 @@ def search_books(search):
     book_data = ap.search(search)
     time.sleep(.5)  # pretend  do some work.
     with print_lock:
-        print("I am the book_data \n", book_data)
+        print("I am the book_search \n", book_data)
 
 
 def search_book_title(search):
     book_data = ap.author_by_name(search)
-    print("title-search ", book_data)
+    with print_lock:
+        print("I am the title_search \n", book_data)
 
 
 def threader():
@@ -40,21 +42,15 @@ for x in range(5):
     author_search = threading.Thread(name='author_search', target=threader)
     # classifying as a daemon, so they will die when the main dies
     book_search.daemon = True
-    # begins, must come after daemon definition
     author_search.daemon = True
-
+    # begins, must come after daemon definition
     book_search.start()
     author_search.start()
 
 start = time.time()
-
-
+# list of random search string- can change it as per your choice
 search_string = ['Computer', 'Love', 'Music', 'Violence']
 for search in search_string:
-    q.put(search)
-
-# 5 jobs assigned.
-for search in range(5):
     q.put(search)
 
 # wait until the thread terminates.
